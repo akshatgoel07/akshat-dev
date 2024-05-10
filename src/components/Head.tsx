82958ab10479f08522749b4522481c839aa816d0
akshato7 <akshathg7@gmail.com>
2024-05-10 18:03:57 +0530

    updated info

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -24,20 +24,19 @@ const Head = () => {
 				</div>
 			</div>
 			<div className="flex justify-between items-center mt-7">
-				Your Vision – Crafted to Perfection
+				Frontend Focused Full-stack developer
 			</div>
 			<div className="mt-5 text-base">
-				マック (Mac) Studio is a design-led studio dedicated to
-				partnering with innovative tech startups, small business',
-				brands & companies. Providing top-tier quality services in
-				product design, branding, motion graphics and development.
+				Hi! I am Akshat a designer and developer, My field of interests
+				is quite extensive, ranging from web development and creative
+				coding to motion design, and UI/UX.
 			</div>
-			<div className="mt-5">
+			{/* <div className="mt-5">
 				Our team is made up of world-class designers and developers from
 				all over the world, from Paris to California to Bangalore. This
 				global perspective allows us to bring a unique blend of ideas
 				and experiences to our projects.
-			</div>
+			</div> */}
 		</div>
 	);
 };
--- src/components/What.tsx
+++ src/components/What.tsx
@@ -4,8 +4,8 @@ import Tedx from "../assets/tedx.png";
 const What = () => {
 	return (
 		<div className=" justify-between items-center mt-12 ">
-			<p className="text-lg">So What do we Do?</p>
-			<p className="text-base mt-4">We make designs that inspire </p>
+			<p className="text-lg">Work</p>
+			{/* <p className="text-base mt-4">We make designs that inspire </p> */}
 			<div
 				style={{
 					borderWidth: ".5px",
@@ -21,8 +21,9 @@ const What = () => {
 				</div>
 			</div>
 			<p className="text-sm mt-4 text-[#BFBFC0]">
-				Logo design techkareer Startup that help hire developers for
-				Multi national corporations valued over $28B.
+				Designed Logo for a tech recruitment platform specializing in
+				connecting highly skilled developers with multinational
+				corporations exceeding $28 billion in valuation.
 			</p>
 			<div
 				style={{
@@ -39,7 +40,7 @@ const What = () => {
 				</div>
 			</div>
 			<p className="text-sm mt-4 text-[#BFBFC0]">
-				We shaped TEDx Horizon's vision by creating its brand identity,
+				Shaped TEDx Horizon's vision by creating its brand identity,
 				motion design, development of the event's website.
 			</p>
 		</div>
