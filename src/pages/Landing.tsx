e6d39d8d99e46484500fe03f55f376276a0ae5f1
akshato7 <akshathg7@gmail.com>
2024-05-22 01:16:29 +0530

    sync

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,7 +3,7 @@ import Meteors from "../components/meteors";
 const Landing = () => {
 	return (
 		<div className="fixed top-0 left-0 w-full h-screen bg-black">
-			<div className=" relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-background p-20 md:shadow-xl">
+			<div className=" relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg bg-background p-20 md:shadow-xl">
 				<Meteors number={30} />
 				<p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white">
 					WIP
